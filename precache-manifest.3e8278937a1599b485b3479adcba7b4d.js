505d5fdc3a3dd7e106c0feaaf77986c4c1229bf1
IvanPustovit <pustovit.for@gmail.com>
2020-04-09 18:46:13 +0300

    Updates

---
+++ precache-manifest.3e8278937a1599b485b3479adcba7b4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2314a360f4394dbfa8c3fc343c9d1ccd",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "9be90e5dba9af95eb5f8",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.dd85e318.chunk.css"
+  },
+  {
+    "revision": "bfc8947838820af559c5",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.e83e872f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.e83e872f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9be90e5dba9af95eb5f8",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.e935219d.chunk.js"
+  },
+  {
+    "revision": "a4572e51e5f015216a2d",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d9659c42.js"
+  }
+]);
